c9c3a3c789ca87a26f4680e2f5546f16650391f6
bhchanuka <b.g.h.chanuka@gmail.com>
2026-01-07 20:22:20 +0530

    Add modal imports to POSScreen

--- src/pos/POSScreen.jsx
+++ src/pos/POSScreen.jsx
@@ -4,3 +4,13 @@ import BillPanel from './components/BillPanel';
 import ControlPanel from './components/ControlPanel';
 import ProductGrid from './components/ProductGrid';
 import { posService } from '../services/posService'; // IMPORT SERVICE
+
+// MODALS
+import PriceCheckModal from './modals/PriceCheckModal';
+import LoyaltyModal from './modals/LoyaltyModal';
+import IOModal from './modals/IOModal';
+import RegisterModal from './modals/RegisterModal';
+import ConfirmModal from './modals/ConfirmModal';
+import ListModal from './modals/ListModal';
+import FloatModal from './modals/FloatModal'; 
+import EndShiftModal from './modals/EndShiftModal';
